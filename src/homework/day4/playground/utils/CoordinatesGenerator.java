b7ed7229b61bdb3e5b834f29fd4fadf1358d1844
Victoria Rudenkova <viktoria.rudenkova@yandex.by>
2023-10-15 20:33:02 +0300

    Small fix for task5

--- src/homework/day4/playground/utils/CoordinatesGenerator.java
+++ src/homework/day4/playground/utils/CoordinatesGenerator.java
@@ -6,7 +6,7 @@ public class CoordinatesGenerator {
     public static int generateCoordinate() {
         Random random = new Random();
         int a = random.nextInt(79) + 1;
-        String str = String.format("CoordinatesGenerator: I have generated point with value: %d", a)
+        String str = String.format("CoordinatesGenerator: I have generated point with value: %d", a);
         System.out.println(str);
         return a;
 
