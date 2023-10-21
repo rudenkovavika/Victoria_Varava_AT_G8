e9900ebf191e92dfc5e220141da12c56c28d2f51
Victoria Rudenkova <viktoria.rudenkova@yandex.by>
2023-10-21 12:05:49 +0300

    I have finished task 1.1 (day 5)

---
+++ src/homework/day5/StringRunner.java
@@ -0,0 +1,8 @@
+package homework.day5;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        PrintToColumn printToColumn = new PrintToColumn();
+        printToColumn.printColumn("Выведи текст по символам в столбик");
+    }
+}
